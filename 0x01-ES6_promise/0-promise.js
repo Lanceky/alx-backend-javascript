60ebbeea4ac676f73a1970f5f685a12890d798c1
Lanceky <evanslance100@gmial.com>
2024-10-01 19:43:07 +0000

    create mode 0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // This Promise doesn't do anything specific
+    // It's just an example of returning a Promise
+  });
+}
+
+export default getResponseFromAPI;
