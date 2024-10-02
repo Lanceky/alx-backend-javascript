576a3bf6d29dd944ce7ebd9663d3672408e177c9
Lanceky <evanslance100@gmial.com>
2024-10-02 19:43:56 +0000

    refactored 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // This Promise doesn't do anything specific
-    // It's just an example of returning a Promise
+    // This Promise doesn't do anything specific yet, 
+    // it's just a simple Promise that resolves immediately
+    resolve();
   });
 }
 
