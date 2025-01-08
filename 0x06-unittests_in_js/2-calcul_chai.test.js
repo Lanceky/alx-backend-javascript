a634b1cfc2c968783d77f1bb552eaa34c64571ea
Lanceky <evanslance100@gmail.com>
2025-01-08 13:42:33 +0000

    useing chai assertion library with expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,21 +2,26 @@ const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-  describe('SUM', () => {
-    it('should return the sum of two numbers', () => {
+  describe('type = SUM', () => {
+    it('should return the sum of two rounded numbers', () => {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      expect(calculateNumber('SUM', 2.4, 3.5)).to.equal(6);
+      expect(calculateNumber('SUM', -1.4, 4.5)).to.equal(4);
     });
   });
 
-  describe('SUBTRACT', () => {
-    it('should return the subtraction of two numbers', () => {
+  describe('type = SUBTRACT', () => {
+    it('should return the subtraction of two rounded numbers', () => {
       expect(calculateNumber('SUBTRACT', 5.4, 3.2)).to.equal(2);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      expect(calculateNumber('SUBTRACT', -1.4, 4.5)).to.equal(-6);
     });
   });
 
-  describe('DIVIDE', () => {
-    it('should return the division of two numbers', () => {
+  describe('type = DIVIDE', () => {
+    it('should return the division of two rounded numbers', () => {
       expect(calculateNumber('DIVIDE', 7.4, 3.2)).to.equal(2.5);
+      expect(calculateNumber('DIVIDE', 9.4, 4.5)).to.equal(2);
     });
 
     it('should return "Error" when dividing by 0', () => {
@@ -24,7 +29,9 @@ describe('calculateNumber', () => {
     });
   });
 
-  it('should throw an error for invalid operation type', () => {
-    expect(() => calculateNumber('INVALID', 1, 2)).to.throw('Invalid operation type');
+  describe('Invalid type', () => {
+    it('should throw an error for invalid operation type', () => {
+      expect(() => calculateNumber('INVALID', 1, 2)).to.throw('Invalid operation type');
+    });
   });
 });
