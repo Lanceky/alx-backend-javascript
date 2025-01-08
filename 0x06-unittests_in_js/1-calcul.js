5da2a42c921753d1e91ee91b402500c295fcf0fd
Lanceky <evanslance100@gmail.com>
2025-01-08 13:54:18 +0000

    update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,17 +2,18 @@ function calculateNumber(type, a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
 
-  if (type === 'SUM') {
-    return roundedA + roundedB;
-  } else if (type === 'SUBTRACT') {
-    return roundedA - roundedB;
-  } else if (type === 'DIVIDE') {
-    if (roundedB === 0) {
-      return 'Error';
-    }
-    return roundedA / roundedB;
-  } else {
-    throw new Error('Invalid operation type');
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    default:
+      throw new Error('Invalid operation type');
   }
 }
 
