b8abf45c6cbfaa5e0cf4fd946b54b3cb9b1926ea
Lanceky <evanslance100@gmail.com>
2025-01-08 06:29:05 +0000

    set status code to 200 and remove trailing spacess"

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ALX!\n');
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello ALX!');
 });
 
 app.listen(1245);
