560158ee7f4c1875a982d2c9b06567115c754a65
Lanceky <evanslance100@gmail.com>
2025-01-08 06:38:30 +0000

    removed extra configurations

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,11 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello ALX!');
-  res.end();
+  res.end('Hello ALX!');
 });
 
-module.exports = app;
+app.listen(1245);
 
-if (require.main === module) {
-  app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-}
+module.exports = app;
