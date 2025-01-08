1ebea6a48cd0a74d2882c7567bbe93fd8959cdc9
Lanceky <evanslance100@gmail.com>
2025-01-08 12:57:18 +0000

    lint compliance

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,10 +2,11 @@ const http = require('http');
 const { readFile } = require('fs').promises;
 
 const countStudents = async (path) => {
+  if (!path) {
+    throw new Error('Cannot load the database');
+  }
+
   try {
-    if (!path) {
-      throw new Error('Cannot load the database');
-    }
     const data = await readFile(path, 'utf8');
     const lines = data.trim().split('\n').slice(1);
     const students = {};
@@ -36,28 +37,26 @@ const countStudents = async (path) => {
 };
 
 const app = http.createServer(async (req, res) => {
-  const url = req.url;
+  const { url } = req;
 
-  if (url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!');
-  } else if (url === '/students') {
-    try {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  try {
+    if (url === '/') {
+      res.end('Hello ALX!');
+    } else if (url === '/students') {
       const data = await countStudents(process.argv[2]);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(data);
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(error.message);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
     }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end(error.message);
   }
 });
 
-const port = 1245;
-
-app.listen(port);
+app.listen(1245);
 
 module.exports = app;
