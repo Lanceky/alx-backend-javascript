250cd812644f881563ee5879448f36ff0edee9fc
Lanceky <evanslance100@gmail.com>
2025-01-08 17:08:16 +0000

    creates a basic express server

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,17 @@
+t express = require('express');
+const app = express();
+
+const PORT = 7865;
+
+// Root route
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the payment system');
+//     });
+//
+//     // Start the server
+//     app.listen(PORT, () => {
+//         console.log(`API available on localhost port ${PORT}`);
+//         });
+//
+//         module.exports = app;
+//
