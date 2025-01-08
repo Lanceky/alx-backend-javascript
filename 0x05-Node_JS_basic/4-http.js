46c261143cd429f5d9dabdaa91bd0e0a08fba04b
Lanceky <evanslance100@gmail.com>
2025-01-08 19:32:58 +0000

    added specific hostname

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,35 @@
-const http = require('http');
+const request = require('request');
+const { expect } = require('chai');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+describe('Index page', () => {
+  const options = {
+    url: 'http://localhost:1245',
+    method: 'GET',
+  };
+
+  it('returns correct status code', (done) => {
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct content', (done) => {
+    request(options, (error, response, body) => {
+      expect(body).to.equal('Hello ALX!');
+      done();
+    });
+  });
+
+  it('returns correct content for /test', (done) => {
+    const testOptions = {
+      url: 'http://localhost:1245/test',
+      method: 'GET',
+    };
+    request(testOptions, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Hello ALX!');
+      done();
+    });
+  });
 });
-
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
-});
-
-module.exports = app;
