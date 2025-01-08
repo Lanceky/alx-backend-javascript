8648385d0b7b3688c57aec41f77140b9945baa90
Lanceky <evanslance100@gmail.com>
2025-01-08 17:10:55 +0000

    used correct server startup code

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,17 +1,16 @@
-t express = require('express');
+// 8-api/api.js
+const express = require('express');
 const app = express();
-
 const PORT = 7865;
 
 // Root route
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the payment system');
-//     });
-//
-//     // Start the server
-//     app.listen(PORT, () => {
-//         console.log(`API available on localhost port ${PORT}`);
-//         });
-//
-//         module.exports = app;
-//
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
