63b514ca94e4c9732c170002fb4e5cf578d79182
Lanceky <evanslance100@gmail.com>
2025-01-08 06:36:50 +0000

    separated the server from listening logic

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,19 @@
 const http = require('http');
 
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello ALX!');
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello ALX!');
+  res.end();
 });
 
-app.listen(1245);
-
 module.exports = app;
+
+if (require.main === module) {
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
