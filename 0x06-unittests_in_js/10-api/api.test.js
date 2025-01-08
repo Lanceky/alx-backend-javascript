90757c2570b8878eef920619e3dc5291126bcfe5
Lanceky <evanslance100@gmail.com>
2025-01-08 19:06:22 +0000

    update test case'

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,84 +1,37 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+// api.test.js
+const request = require('supertest');
 const app = require('./api');
 
-chai.use(chaiHttp);
-const { expect } = chai;
-
-describe('API Tests', () => {
-    describe('GET /', () => {
-        it('should return welcome message', (done) => {
-            chai.request(app)
-                .get('/')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.text).to.equal('Welcome to the payment system');
-                    done();
-                });
-        });
+describe('Test /available_payments endpoint', () => {
+  it('should return the correct payment methods', async () => {
+    const response = await request(app).get('/available_payments');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      payment_methods: {
+        credit_cards: true,
+        paypal: false
+      }
     });
+  });
+});
 
-    describe('GET /cart/:id', () => {
-        it('should return payment methods for valid cart id', (done) => {
-            chai.request(app)
-                .get('/cart/123')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.text).to.equal('Payment methods for cart 123');
-                    done();
-                });
-        });
+describe('Test /login endpoint', () => {
+  it('should return welcome message with username', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({ userName: 'Betty' })
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Welcome Betty');
+  });
 
-        it('should return 400 for invalid cart id', (done) => {
-            chai.request(app)
-                .get('/cart/abc')
-                .end((err, res) => {
-                    expect(res).to.have.status(400);
-                    expect(res.text).to.equal('Invalid cart id');
-                    done();
-                });
-        });
-    });
-
-    describe('GET /available_payments', () => {
-        it('should return payment methods object', (done) => {
-            chai.request(app)
-                .get('/available_payments')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.deep.equal({
-                        payment_methods: {
-                            credit_cards: true,
-                            paypal: false,
-                        },
-                    });
-                    done();
-                });
-        });
-    });
-
-    describe('POST /login', () => {
-        it('should return welcome message with username', (done) => {
-            chai.request(app)
-                .post('/login')
-                .send({ userName: 'Betty' })
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.text).to.equal('Welcome Betty');
-                    done();
-                });
-        });
-
-        it('should return 400 if userName is not provided', (done) => {
-            chai.request(app)
-                .post('/login')
-                .send({})
-                .end((err, res) => {
-                    expect(res).to.have.status(400);
-                    expect(res.text).to.equal('Bad Request');
-                    done();
-                });
-        });
-    });
+  it('should return 400 status for missing username', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({})
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toBe('Missing userName');
+  });
 });
 
