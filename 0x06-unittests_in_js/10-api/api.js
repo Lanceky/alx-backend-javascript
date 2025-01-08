626a2cf95a5dd05f9009e2ccaa86428a769ddd2a
Lanceky <evanslance100@gmail.com>
2025-01-08 19:10:50 +0000

    updated api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -7,6 +7,21 @@ app.use(bodyParser.json());
 
 const PORT = 7865;
 
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+// Route to handle cart payment methods
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    res.status(400).send('Invalid cart ID');
+  } else {
+    res.send(`Payment methods for cart ${id}`);
+  }
+});
+
 // Endpoint to get available payment methods
 app.get('/available_payments', (req, res) => {
   res.json({
@@ -17,7 +32,7 @@ app.get('/available_payments', (req, res) => {
   });
 });
 
-// Endpoint to login
+// Endpoint to handle login
 app.post('/login', (req, res) => {
   const { userName } = req.body;
   if (userName) {
@@ -27,9 +42,9 @@ app.post('/login', (req, res) => {
   }
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
-
