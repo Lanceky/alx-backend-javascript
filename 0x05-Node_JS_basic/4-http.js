675d6cd4afbd5b036edc834ad4ed7f373c8f9336
Predator <evanslance100@gmail.com>
2025-01-28 16:46:36 +0300

    replace content type with text/plain

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
 const {createServer} = require('node:http');
 
-const hostname = 'localhost';
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = createServer((req, res)=>{
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'plain/text')
+  res.setHeader('Content-Type', 'text/plain')
   res.end('Hello ALX!');
 });
 
