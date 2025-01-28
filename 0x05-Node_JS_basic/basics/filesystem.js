1d3ec361616fd17e0cfee703075554f83f089f64
Predator <evanslance100@gmail.com>
2025-01-28 16:07:12 +0300

    delete swap file

---
+++ 0x05-Node_JS_basic/basics/filesystem.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const fileContent = fs.readFileSync(path, 'utf-8');
+    const lines = fileContent.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line.length > 0);
+    const fields = {};
+    console.log(`Number of students: ${students.length}`);
+    students.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = { count: 0, names: [] };
+      }
+      fields[field].count += 1;
+      fields[field].names.push(firstName);
+    });
+    Object.entries(fields).forEach(([field, data]) => {
+      console.log(`Number of students in ${field}: ${data.count}. List: ${data.names.join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
+const path = './database.csv';  // Relative path to my db
+countStudents(path);
