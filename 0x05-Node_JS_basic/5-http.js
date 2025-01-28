a0070b12e647e58642be796ad6ab942756dbb2fc
Predator <evanslance100@gmail.com>
2025-01-28 17:42:02 +0300

    Fix implicit-arrow-linebreak ESLint error

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,36 +2,35 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const countStudents = (filePath) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.trim().split('\n').filter((line) => line.length > 0);
-        const students = lines.slice(1); // Skip the header row
-        const fields = {};
+const countStudents = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      const lines = data.trim().split('\n').filter((line) => line.length > 0);
+      const students = lines.slice(1); // Skip the header row
+      const fields = {};
 
-        students.forEach((student) => {
-          const [firstName, , , field] = student.split(',');
-          if (!fields[field]) {
-            fields[field] = { count: 0, names: [] };
-          }
-          fields[field].count += 1;
-          fields[field].names.push(firstName);
-        });
+      students.forEach((student) => {
+        const [firstName, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = { count: 0, names: [] };
+        }
+        fields[field].count += 1;
+        fields[field].names.push(firstName);
+      });
 
-        const summary = [`Number of students: ${students.length}`];
-        Object.entries(fields).forEach(([field, data]) => {
-          summary.push(
-            `Number of students in ${field}: ${data.count}. List: ${data.names.join(', ')}`,
-          );
-        });
+      const summary = [`Number of students: ${students.length}`];
+      Object.entries(fields).forEach(([field, data]) => {
+        summary.push(
+          `Number of students in ${field}: ${data.count}. List: ${data.names.join(', ')}`,
+        );
+      });
 
-        resolve(summary.join('\n'));
-      }
-    });
+      resolve(summary.join('\n'));
+    }
   });
+});
 
 const app = http.createServer((req, res) => {
   const filePath = path.resolve(__dirname, process.argv[2]);
