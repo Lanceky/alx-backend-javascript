083a3f402f6e0dac48e832434ddf5a2ea86b4766
Predator <evanslance100@gmail.com>
2025-01-28 17:09:09 +0300

    Fix server response to comply with requirements

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,12 +2,10 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello ALX!');
   } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
