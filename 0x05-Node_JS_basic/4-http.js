8e9d26c7c1c25dbff00060a9e4524480d54ae1d1
Predator <evanslance100@gmail.com>
2025-01-28 17:22:03 +0300

    The response and headers are configured identically for all endpoints.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
 });
 
 app.listen(1245);
